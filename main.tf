50fbd2d1695aee4c06f82fe904d91b0e0197af71
Javier Meiriño <jjmeiras@gmail.com>
2019-05-03 12:03:44 +0200

    Added .tf files

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.region}"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "dedicated"
+
+  tags = {
+    Name = "Jenkins_Example"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "mybucket"
+    key    = "path/to/my/key"
+    region = "us-east-1"
+  }
+}
