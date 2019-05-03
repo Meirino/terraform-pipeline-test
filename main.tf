afbbb08bc581b46f9251548089ef78b9a444fe4d
Javier Meiriño <jjmeiras@gmail.com>
2019-05-03 13:46:09 +0200

    Eliminado remote

--- main.tf
+++ main.tf
@@ -13,10 +13,10 @@ resource "aws_vpc" "main" {
   }
 }
 
-terraform {
-  backend "s3" {
-    bucket = "mybucket"
-    key    = "path/to/my/key"
-    region = "us-east-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "mybucket"
+#     key    = "path/to/my/key"
+#     region = "us-east-1"
+#   }
+# }
