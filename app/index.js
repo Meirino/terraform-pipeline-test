9d88edb9d4fc44b8a140da18971e94d7434da1c4
José Javier Meiriño Migens <jjmeirino@gmail.com>
2019-05-08 10:09:29 +0200

    Added static html serving

--- app/index.js
+++ app/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.configure(() => {
+    app.use(express.static(__dirname + '/static'));
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
