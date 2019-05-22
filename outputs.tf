b2ff8c5027168f2710f2a4fed05b39bf9a338cb9
José Javier Meiriño Migens <jjmeirino@gmail.com>
2019-05-22 11:14:29 +0200

    Añadido remote outputs a Terradorm

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "Web_ip" {
 output "Web2_ip" {
   value = "${aws_instance.web2.public_ip}"
 }
+
+output "Web_ip_remote" {
+  value = "${data.terraform_remote_state.vpc.Web_ip}"
+}
+
+output "Web2_ip_remote" {
+  value = "${data.terraform_remote_state.vpc.Web2_ip}"
+}
