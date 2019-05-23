a2d5eb6f84aed68e9febc57ec1143f0b89b75d69
José Javier Meiriño Migens <jjmeirino@gmail.com>
2019-05-23 08:13:14 +0200

    Añadido outputs al remote

--- outputs.tf
+++ outputs.tf
@@ -12,9 +12,9 @@ output "Web2_ip" {
 
 // El remote falla si no exiten los atributos (O no han sido creados aún)
 
-output "VPC_id_remote" {
-  value = "${data.terraform_remote_state.cbgi.VPC_id}"
-}
+# output "VPC_id_remote" {
+#   value = "${data.terraform_remote_state.cbgi.VPC_id}"
+# }
 
 output "Web_ip_remote" {
   value = "${data.terraform_remote_state.cbgi.Web_ip}"
